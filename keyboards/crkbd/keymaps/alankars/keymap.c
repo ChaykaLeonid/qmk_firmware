4bc2d366470e9a691539b72555a52405f4f1f802
ChaykaLeonid <chaykaleonid@gmail.com>
2024-04-28 13:41:00 +0300

    alankars

--- keyboards/crkbd/keymaps/alankars/keymap.c
+++ keyboards/crkbd/keymaps/alankars/keymap.c
@@ -23,11 +23,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
      KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
- LCTL(KC_VOLU), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
+ LCTL_T(KC_VOLU), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
- LSFT(KC_VOLD), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ESC,
+ LSFT_T(KC_VOLD), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ESC,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                        LGUI_T(KC_MUTE) , KC_SPC  , LT(1, KC_MRWD) ,    LT(2, KC_MPLY) ,  KC_ENT,  LALT_T(KC_MFFD)
+                        LGUI_T(KC_MUTE) , KC_SPC  , LT(1, KC_MPRV) ,    LT(2, KC_MPLY) ,  KC_ENT,  LALT_T(KC_MNXT)
                                       //`--------------------------'  `--------------------------'
 
   ),
@@ -46,11 +46,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [2] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      KC_TAB ,  KC_AT , KC_TILD, KC_LCBR, KC_RCBR, KC_UNDS,                      KC_PLUS,  KC_P7 ,  KC_P8 , KC_P9  , KC_CIRC, KC_BSPC,
+      KC_TAB ,  KC_AT , KC_TILD, KC_LCBR, KC_RCBR, KC_UNDS,                      KC_PLUS,  KC_7  ,  KC_8  ,  KC_9  , KC_CIRC, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL, KC_PIPE, KC_AMPR, KC_LPRN, KC_RPRN, KC_PAST,                      KC_MINS,  KC_P4,   KC_P5 , KC_P6  , KC_BSLS, KC_PEQL,
+      KC_LCTL, KC_PIPE, KC_AMPR, KC_LPRN, KC_RPRN, KC_PAST,                      KC_MINS,  KC_4  ,  KC_5  ,  KC_6  , KC_BSLS, KC_PEQL,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT, KC_HASH, KC_DLR , KC_LBRC, KC_RBRC, KC_EXLM,                       KC_P0 ,  KC_P1 ,  KC_P2 , KC_P3  , KC_PERC, KC_TRNS,
+      KC_LSFT, KC_HASH, KC_DLR , KC_LBRC, KC_RBRC, KC_EXLM,                       KC_0  ,  KC_1  ,  KC_2  ,  KC_3  , KC_PERC, CW_TOGG,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LGUI, KC_SPC ,  MO(3) ,    _______ , KC_ENT, KC_RALT
                                       //`--------------------------'  `--------------------------'
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [3] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_TOG,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      QK_BOOT, XXXXXXX, XXXXXXX, KC_BRID, KC_BRIU, RGB_TOG,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       XXXXXXX, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, RGB_MOD,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
